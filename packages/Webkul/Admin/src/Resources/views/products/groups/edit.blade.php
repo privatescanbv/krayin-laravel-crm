5f948a582bb0f7c0b7d8325baf2d27aed0cea729
mbulthuis <mbulthuis@gmail.com>
2025-05-20 16:53:43 +0200

    - product group heeft een parent - add docs - add IDE helper

--- packages/Webkul/Admin/src/Resources/views/products/groups/edit.blade.php
+++ packages/Webkul/Admin/src/Resources/views/products/groups/edit.blade.php
@@ -1,6 +1,6 @@
 <x-admin::layouts>
     <x-slot:title>
-        @lang('admin::app.products.product-groups.edit.title')
+        @lang('admin::app.productgroups.edit.title')
     </x-slot>
 
     <x-admin::form
@@ -11,7 +11,7 @@
             <div class="flex items-center justify-between rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300">
                 <div class="flex flex-col gap-2">
                     <div class="text-xl font-bold dark:text-white">
-                        @lang('admin::app.products.product-groups.edit.title')
+                        @lang('admin::app.productgroups.edit.title')
                     </div>
                 </div>
 
@@ -20,7 +20,7 @@
                         type="submit"
                         class="primary-button"
                     >
-                        @lang('admin::app.products.product-groups.edit.save-btn')
+                        @lang('admin::app.productgroups.edit.save-btn')
                     </x-admin::button>
                 </div>
             </div>
@@ -28,7 +28,7 @@
             <div class="box-shadow rounded-lg border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-gray-900">
                 <x-admin::form.control-group>
                     <x-admin::form.control-group.label>
-                        @lang('admin::app.products.product-groups.edit.name')
+                        @lang('admin::app.productgroups.edit.name')
                     </x-admin::form.control-group.label>
 
                     <x-admin::form.control-group.control
@@ -36,23 +36,43 @@
                         name="name"
                         :value="old('name', $productGroup->name)"
                         rules="required"
-                        :label="trans('admin::app.products.product-groups.edit.name')"
-                        :placeholder="trans('admin::app.products.product-groups.edit.name')"
+                        :label="trans('admin::app.productgroups.edit.name')"
+                        :placeholder="trans('admin::app.productgroups.edit.name')"
                     />
                 </x-admin::form.control-group>
 
                 <x-admin::form.control-group>
                     <x-admin::form.control-group.label>
-                        @lang('admin::app.products.product-groups.edit.description')
+                        @lang('admin::app.productgroups.edit.description')
                     </x-admin::form.control-group.label>
 
                     <x-admin::form.control-group.control
                         type="textarea"
                         name="description"
                         :value="old('description', $productGroup->description)"
-                        :label="trans('admin::app.products.product-groups.edit.description')"
-                        :placeholder="trans('admin::app.products.product-groups.edit.description')"
+                        :label="trans('admin::app.productgroups.edit.description')"
+                        :placeholder="trans('admin::app.productgroups.edit.description')"
                     />
+                </x-admin::form.control-group>
+
+                <x-admin::form.control-group>
+                    <x-admin::form.control-group.label>
+                        @lang('admin::app.productgroups.edit.parent')
+                    </x-admin::form.control-group.label>
+
+                    <x-admin::form.control-group.control
+                        type="select"
+                        name="parent_id"
+                        :value="old('parent_id', $productGroup->parent_id)"
+                        :label="trans('admin::app.productgroups.edit.parent')"
+                    >
+                        <option value="">@lang('admin::app.productgroups.edit.select-parent')</option>
+                        @foreach ($productGroups as $group)
+                            <option value="{{ $group->id }}" {{ $productGroup->parent_id == $group->id ? 'selected' : '' }}>
+                                {{ $group->name }}
+                            </option>
+                        @endforeach
+                    </x-admin::form.control-group.control>
                 </x-admin::form.control-group>
             </div>
         </div>
--- packages/Webkul/Product/src/Models/ProductGroup.php
+++ packages/Webkul/Product/src/Models/ProductGroup.php
@@ -10,6 +10,7 @@ class ProductGroup extends Model implements ProductGroupContract
     protected $fillable = [
         'name',
         'description',
+        'parent_id',
     ];
 
     /**
@@ -18,5 +19,37 @@ class ProductGroup extends Model implements ProductGroupContract
     public function products()
     {
         return $this->hasMany(ProductProxy::modelClass());
+    }
+
+    /**
+     * Get the parent group.
+     */
+    public function parent()
+    {
+        return $this->belongsTo(ProductGroupProxy::modelClass(), 'parent_id');
+    }
+
+    /**
+     * Get the child groups.
+     */
+    public function children()
+    {
+        return $this->hasMany(ProductGroupProxy::modelClass(), 'parent_id');
+    }
+
+    /**
+     * Get all descendants of the group.
+     */
+    public function descendants()
+    {
+        return $this->children()->with('descendants');
+    }
+
+    /**
+     * Get all ancestors of the group.
+     */
+    public function ancestors()
+    {
+        return $this->parent()->with('ancestors');
     }
 }

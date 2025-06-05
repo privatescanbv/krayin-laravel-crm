1dba8aa9d6470d9b136879be7e8f5aff94daeb53
mbulthuis <mbulthuis@gmail.com>
2025-06-05 09:18:15 +0200

    POC n8n integratie webhook en api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/leads/{leadId}/notes', [App\Http\Controllers\LeadNoteController::class, 'store']);

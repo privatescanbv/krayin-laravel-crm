0eb1dab34893e900a1a481eb4477fc34e798b35e
mbulthuis <mbulthuis@gmail.com>
2025-09-17 13:36:37 +0200

    fix

--- packages/Webkul/Admin/src/Http/Controllers/Contact/Persons/ActivityController.php
+++ packages/Webkul/Admin/src/Http/Controllers/Contact/Persons/ActivityController.php
@@ -30,7 +30,7 @@ class ActivityController extends Controller
      * Display a listing of the resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index($id)
     {

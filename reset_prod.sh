55f79148b232bceb69b7391e255f8cce9bceb98e
mbulthuis <mbulthuis@gmail.com>
2025-09-23 18:03:48 +0200

    fix: increase prod import to 30000

--- reset_prod.sh
+++ reset_prod.sh
@@ -2,8 +2,8 @@
 
 php artisan migrate:fresh --seed &&
 php artisan import:users &&
-php artisan import:persons --limit=10000 &&
-php artisan import:leads --limit=10000 &&
+php artisan import:persons --limit=30000 &&
+php artisan import:leads --limit=30000 &&
 php artisan import:email-attachment-files
 
 

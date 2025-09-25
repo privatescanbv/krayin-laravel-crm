e05c1abd0a6da687460efb12daa3200f06ba91f9
mbulthuis <mbulthuis@gmail.com>
2025-09-25 09:02:24 +0000

    fix: log-viewer and remove logs by start up

--- .docker/start.sh
+++ .docker/start.sh
@@ -9,5 +9,13 @@ php artisan migrate --force || true
 php artisan migrate --path=vendor/laravel/sanctum/database/migrations --force || true
 php artisan storage:link || true
 
+# Ensure log-viewer assets are available (idempotent)
+if [ ! -d "public/vendor/log-viewer" ]; then
+    echo "Publishing log-viewer assets..."
+    php artisan vendor:publish --tag=log-viewer-assets --force || true
+    # remove old logs, by default.
+    rm ./storage/logs/*.log
+fi
+
 echo "Starting Supervisor..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf

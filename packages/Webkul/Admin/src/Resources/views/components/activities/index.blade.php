26e8f9882a8c03d2d58e67f6fd66a4ce3bf8a621
mbulthuis <mbulthuis@gmail.com>
2025-11-06 16:01:44 +0100

    fix: assign to label in activity

--- packages/Webkul/Admin/src/Resources/views/components/activities/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/activities/index.blade.php
@@ -305,7 +305,7 @@
                                         <div class="text-gray-500 dark:text-gray-300">
                                             @{{ $admin.formatDate(activity.created_at, 'd MMM yyyy, h:mm A', timezone) }},
 
-                                            @{{ "@lang('admin::app.components.activities.index.by-user', ['user' => 'replace'])".replace('replace', activity.user?.name ?? '@lang('admin::app.components.activities.index.system')') }}
+                                            Toegewezen aan: @{{ activity.user?.name ?? '-' }}
                                         </div>
 
                                         {!! view_render_event('admin.components.activities.content.activity.item.time_and_user.after') !!}

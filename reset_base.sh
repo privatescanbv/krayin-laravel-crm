e1f85085002c4d66da5795a702fd51d04ac6a4ca
mbulthuis <mbulthuis@gmail.com>
2025-11-24 20:23:11 +0100

    fix: loki

--- reset_base.sh
+++ reset_base.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 APP_ENV="$1"
+# Kies artisan command
+if [[ "$APP_ENV" == "prod" ]]; then
+    ARTISAN="php artisan"
+else
+    ARTISAN="./vendor/bin/sail artisan"
+fi
 
 # Command uitvoeren
 $ARTISAN migrate:fresh &&
 # create realm and capture output to extract the secrets
-./sync_keycloak.sh "$APP_ENV" &&
+./create-realm.sh "$APP_ENV" &&
 $ARTISAN db:seed &&
 $ARTISAN import:users

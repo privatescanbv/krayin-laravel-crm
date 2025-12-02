b97e90a23c0b4c95c322c637f3ceea3fbd77c836
mbulthuis <mbulthuis@gmail.com>
2025-12-02 15:43:17 +0100

    refactor: edit forms with field blade template

--- resources/views/adminc/components/field.blade.php
+++ resources/views/adminc/components/field.blade.php
@@ -6,20 +6,42 @@
     'placeholder' => '',
     'type' => 'text',
     'class' => '',
+    'rules' => null,
+    'labelClass' => '',
 ])
+
+@php
+    // Bepaal control name (voor errors) en label tekst
+    $controlName = $name ?: \Illuminate\Support\Str::slug($label, '_');
+    $labelText = $label ?: $name;
+
+    // Required class uit rules halen als die nog niet expliciet is gezet.
+    $computedLabelClass = $labelClass;
+    if (! $computedLabelClass && $rules && \Illuminate\Support\Str::contains($rules, 'required')) {
+        $computedLabelClass = 'required';
+    }
+@endphp
+
 <x-admin::form.control-group class="{{ $class }}">
     <x-admin::form.control-group.control
         type="{{ $type }}"
         name="{{ $name ?: $label }}"
         value="{{ $value }}"
-        label="{{ $label ?: $name }}"
+        label="{{ $labelText }}"
         placeholder="{{ $placeholder }}"
-        :readonly="$readonly" />
+        :readonly="$readonly"
+        :rules="$rules"
+    >
+        {{-- Doorlaat slot zodat select/textarea/options gebruikt kunnen worden --}}
+        {{ $slot }}
+    </x-admin::form.control-group.control>
 
     <x-admin::form.control-group.label
-        :switch="$type === 'switch'">
-        {{ $label ?: $name }}
+        :switch="$type === 'switch'"
+        class="{{ $computedLabelClass }}"
+    >
+        {{ $labelText }}
     </x-admin::form.control-group.label>
 
-    <x-admin::form.control-group.error control-name="{{ $name }}" />
+    <x-admin::form.control-group.error control-name="{{ $controlName }}" />
 </x-admin::form.control-group>

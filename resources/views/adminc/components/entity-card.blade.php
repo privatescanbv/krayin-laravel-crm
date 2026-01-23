a7f042bed4f576009f6985d86d15fcf04ccf36bf
mbulthuis <mbulthuis@gmail.com>
2026-01-23 17:00:19 +0100

    fix: view lead

--- resources/views/adminc/components/entity-card.blade.php
+++ resources/views/adminc/components/entity-card.blade.php
@@ -144,6 +144,9 @@
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                   d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                                         </svg>
+                                        @if (!empty($phone['value']))
+                                            <span class="contact-label">{{ $phone['value'] }}</span>
+                                        @endif
                                         @if (!empty($phone['label']))
                                             <span class="contact-label">({{ $phone['label'] }})</span>
                                         @endif
@@ -162,9 +165,8 @@
                                                   d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                                         </svg>
                                         @if (!empty($email['value']))
-                                            <span class="contact-label">({{ $email['value'] }})</span>
+                                            <span class="contact-label">{{ $email['value'] }}</span>
                                         @endif
-                                        {{ $email['value'] ?? '' }}
                                         @if (isset($email['label']) && $email['label'])
                                             <span class="contact-label">({{ $email['label'] }})</span>
                                         @endif

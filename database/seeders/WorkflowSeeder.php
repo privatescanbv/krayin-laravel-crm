a1cc120e19e69d8a1253c5fec187a360b43fb274
markk <mark.klaucke@digi4you.nl>
2026-02-26 13:36:50 +0100

    feat:verwijderen order bevestigd

--- database/seeders/WorkflowSeeder.php
+++ database/seeders/WorkflowSeeder.php
@@ -53,7 +53,6 @@ class WorkflowSeeder extends Seeder
             PipelineStage::KLANT_ADVISEREN_WACHTEN_OP_MRI_HERNIA    => ['Klant levert MRI beelden aan, verwerken', $defaultDescription],
             PipelineStage::KLANT_ADVISEREN_MRI_BINNEN_HERNIA        => ['Klant adviseren met MRI beelden', $defaultDescription],
             PipelineStage::SALES_DOC_COMPLETE_HERNIA                => ['4.1 consult met arts', $defaultDescription],
-            PipelineStage::ORDER_BEVESTIGD                          => ['Order bevestigen', $defaultDescription],
             PipelineStage::SALES_MET_SUCCES_AFGEROND                => ['Test auto op sales entiteit', $defaultDescription],
 
             default => ["Auto-activity: {$stage->name()}", $defaultDescription],

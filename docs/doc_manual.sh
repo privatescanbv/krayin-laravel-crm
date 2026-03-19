f58e8e1bf6e94fe1c69189855be47037a22839b1
mbulthuis <mbulthuis@gmail.com>
2026-03-19 14:01:34 +0100

    feat: first setup for user manual

--- docs/doc_manual.sh
+++ docs/doc_manual.sh
@@ -3,4 +3,5 @@
 # Creates manual for the customers of Lento. This bash file isn't executed in CI, so needs to be done manually.
 # output: See ./modules/application/pages/manual.pdf
 
-asciidoctor-pdf ./modules/application/pages/manual.adoc
+#asciidoctor-pdf ./modules/application/pages/manual.adoc
+asciidoctor-pdf ./manual/leads-handleiding.adoc -o ./manual/leads_manual.pdf

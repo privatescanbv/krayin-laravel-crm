3a2aa4df47847c6f98947773fd846d32656319d7
mbulthuis <mbulthuis@gmail.com>
2026-09-22 14:42:59 +0200

    fix: keycloak local development

--- .docker/keycloak/docker-entrypoint.sh
+++ .docker/keycloak/docker-entrypoint.sh
@@ -8,5 +8,7 @@ fi
 sed -i "s|\${env.FORMS_FRONTEND_URL}|${FORMS_FRONTEND_URL:-}|g" \
     /opt/keycloak/themes/privatescan/login/theme.properties
 
-/opt/keycloak/bin/kc.sh build --features=token-exchange
+# Impersonation uses requested_subject (legacy token-exchange:v1).
+# Keycloak 26.2+ enables Standard TE by default, which rejects requested_subject.
+/opt/keycloak/bin/kc.sh build --features=token-exchange:v1 --features-disabled=token-exchange-standard
 exec /opt/keycloak/bin/kc.sh start --optimized
